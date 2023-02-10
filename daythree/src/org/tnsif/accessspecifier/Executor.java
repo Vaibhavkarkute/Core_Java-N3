d7a451025b7613b4e84139b4e2f703a7b2b75c81
Vaibhav Karkute <vaibhav.karkute007@gmail.com>
2023-02-10 23:16:46 +0530

    first commit

---
+++ daythree/src/org/tnsif/accessspecifier/Executor.java
@@ -0,0 +1,13 @@
+package org.tnsif.accessspecifier;
+
+import org.tnsif.accessspecifierdemo.AccessSpecifierExample;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		AccessSpecifierExample e=new AccessSpecifierExample();
+		e.setName("Vaibhav");
+		e.display();
+	}
+
+}
