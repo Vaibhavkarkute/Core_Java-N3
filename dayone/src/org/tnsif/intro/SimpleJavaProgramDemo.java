be0ab6590787bf158f599b42232867eaee57718b
Vaibhav Karkute <vaibhav.karkute007@gmail.com>
2023-02-10 22:59:26 +0530

    first commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.intro;
+
+//simple java program to print a line
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Fullstack Program");
+		
+
+	
+	}
+
+}
