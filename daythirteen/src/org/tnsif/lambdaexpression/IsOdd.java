629d5958cab160e06589de0e01d4974483762d72
Vaibhav Karkute <101312171+Vaibhavkarkute@users.noreply.github.com>
2023-02-26 23:08:11 +0530

    first commit

---
+++ daythirteen/src/org/tnsif/lambdaexpression/IsOdd.java
@@ -0,0 +1,8 @@
+package org.tnsif.lambdaexpression;
+//program to demonstrate on lambda expression to implement functional interface
+//follow annotation ensures that interface contain only one abstract method
+@FunctionalInterface
+public interface IsOdd {
+	boolean checkOdd(int a);
+    
+}
