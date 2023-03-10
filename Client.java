01a4f008fa22ad632ed0a2c4a178dc60c7cd1f3b
Vaibhav Karkute <101312171+Vaibhavkarkute@users.noreply.github.com>
2023-03-10 21:58:00 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -9,8 +9,8 @@ public class Client {
 
 	public static void main(String[] args) {
 		BankFactory f = new MMBankFactory();
-		MMSavingAcc s = new MMSavingAcc(1845, "virat", 40000.0f, true);
-		MMCurrentAcc c = new MMCurrentAcc(4518, "Hardik", 60000.0f,70000.0f);
+		MMSavingAcc s = new MMSavingAcc(101, "vruah", 40000.0f, true);
+		MMCurrentAcc c = new MMCurrentAcc(102, "saurav", 60000.0f,70000.0f);
 		
 		System.out.println("Savings Account:");
 		System.out.println(s);
